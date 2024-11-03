526835bc0886282e8bd454cd4b33fbca00ae1dfa
rajkiran jaiswar <109668455+Rajkiran-125@users.noreply.github.com>
2024-11-03 13:14:54 +0530

    app.js file change

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-const path = require('path');
+let express = require('express');
+let app = express();
+
+let http = require('http');
+let server = http.Server(app);
+
+let socketIO = require('socket.io');
+let io = socketIO(server);
 
 let socketConnected = new Set();
 
-const server = app.listen(port, () => {
-    console.log('server is running on port', port);
-});
+const port = process.env.PORT || 3000;
 
-const io = require('socket.io')(server);
+io.on('connection', (socket) => {
+    socket.on('join', (data) => {
+        socket.join(data.room);
+        socket.broadcast.to(data.room).emit('user joined');
+    });
 
-app.use(express.static((path.join(__dirname, 'public'))));
+    console.log('Socket Id',socket.id);
 
-io.on('connection', onConnected);
-
-function onConnected(socket){
-    console.log('Socket Id: ', socket.id);
-    socketConnected.add(socket.id);
+    socket.on('message', (data) => {
+        io.in(data.room).emit('new message', {user: data.user, message: data.message}, console.log('Message: >>> ',data.message));
+    });
 
     io.emit('clients-total', socketConnected.size);
-    
+
     socket.on('disconnect', ()=>{
         console.log('Disconnected socket: ', socket.id);
         socketConnected.delete(socket.id);
         io.emit('clients-total', socketConnected.size);
-    })
-}
+    });
+
+});
+
+server.listen(port, () => {
+    console.log(`started on port: ${port}`);
+});
